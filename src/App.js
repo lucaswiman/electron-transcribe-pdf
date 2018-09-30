f0818037554abd1781604d3abed6b01cc5293f87
Lucas Wiman <lucaswiman@counsyl.com>
2018-09-29 21:30:33 -0700

    Actually get the PDF to render on a canvas using react.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyPdfViewer  from './pdf.js';
+
+const fs = window.require('fs');
+const remote = window.require('electron').remote;
+const dialog = remote.dialog;
+
 
 class App extends Component {
+  state = {
+    renderedPDF: null,
+  };
   render() {
     return (
       <div className="App">
@@ -13,9 +22,23 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <a onClick={this.renderPDF}>Load</a>
+        {this.state.renderedPDF}
       </div>
     );
   }
+  renderPDF = () => {
+    const fileNames = dialog.showOpenDialog({properties: ['openFile']});
+    if (typeof fileNames == 'undefined') {
+      return;
+    }
+    const fileName = fileNames[0];
+    console.log(fileName);
+    window.yourMom = fs;
+    const buffer = fs.readFileSync(fileName);
+
+    this.setState({renderedPDF: <MyPdfViewer file={buffer} />});
+  };
 }
 
 export default App;
